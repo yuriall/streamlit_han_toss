642f27c1aa66e3e8793af092b8498b3e1e0ee1f4
yuriall <ba203dldbfl#gmail.com>
2024-07-15 10:13:56 +0900

    data

---
+++ EDA.py
@@ -0,0 +1,14 @@
+import string
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+from wordcloud import WordCloud
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from scipy.sparse import save_npz
+import warnings
+warnings.filterwarnings('ignore')
--- app.py
+++ app.py
@@ -35,3 +35,6 @@ map_data = pd.DataFrame(
     np.random.randn(100, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 st.map(map_data)
+
+titanic = pd.read_csv("./data/titanic.csv")
+st.write(titanic)
